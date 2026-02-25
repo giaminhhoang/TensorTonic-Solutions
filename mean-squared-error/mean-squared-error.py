aeb5879cc742874fece123aa17cec4f1f60628d4
Minh Hoang <hoanggminh@gmail.com>
2026-02-25 13:41:16 +0100

    Add solution for mean-squared-error

---
+++ mean-squared-error/mean-squared-error.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def mean_squared_error(y_pred, y_true):
+    """
+    Returns: float MSE
+    """
+    # Write code here
+    y_pred = np.asarray(y_pred, dtype=float)
+    y_true = np.asarray(y_true, dtype=float)
+    mse = np.mean((y_pred - y_true)**2)
+    return mse
