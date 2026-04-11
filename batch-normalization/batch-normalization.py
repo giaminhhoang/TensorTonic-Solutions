b040763bb38f4fada228d5d2c29f5e0ce3bd7edb
Minh Hoang <hoanggminh@gmail.com>
2026-04-12 00:59:03 +0200

    Add solution for batch-normalization

---
+++ batch-normalization/batch-normalization.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def batch_norm_forward(x, gamma, beta, eps=1e-5):
+    """
+    Forward-only BatchNorm for (N,D) or (N,C,H,W).
+    """
+    # Write code here
+    x = np.asarray(x, dtype=np.float64)
+    if x.ndim == 2:
+        mu = np.mean(x, axis=0, keepdims=True)
+        var = np.var(x, axis=0, keepdims=True)
+        x_norm = (x - mu)/np.sqrt(var+eps)
+        y = gamma*x_norm + beta
+    elif x.ndim == 4:
+        mu = np.mean(x, axis=(0,2,3), keepdims=True)
+        var = np.var(x, axis=(0,2,3), keepdims=True)
+        x_norm = (x - mu)/np.sqrt(var+eps)
+        gamma = np.asarray(gamma, dtype=np.float64).reshape(1,-1,1,1)
+        beta = np.asarray(beta, dtype=np.float64).reshape(1,-1,1,1)
+        y = gamma*x_norm + beta
+    return y
